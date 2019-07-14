e6431dcd3448a21ad182154683659903f7b12c1c
Kuba <ja.tanski@gmail.com>
2019-07-14 19:54:36 +0200

    <feat> contact structure is ready

---
+++ portfolio/src/components/contact/ContactDesc/ContactDesc.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import "./contactDesc.scss"
+
+const ContactDesc = () => (
+  <div className="desc">
+    <h1 className="desc__title">Contact & CV</h1>
+    <div className="desc__desc">
+      <p className="desc__desc__el">
+        If you want contact with me, below you have all my media.
+      </p>
+      <p className="desc__desc__el">Also you can download my CV document.</p>
+    </div>
+  </div>
+)
+
+export default ContactDesc
--- portfolio/src/pages/contact.jsx
+++ portfolio/src/pages/contact.jsx
@@ -3,33 +3,19 @@ import { Link } from "gatsby"
 
 import SEO from "../components/seo"
 import Menu from "../components/Menu/Menu"
+import ContactDesc from "../components/contact/ContactDesc/ContactDesc"
+import ContactMedia from "../components/contact/ContactMedia/CoctactMedia"
 
 import "../styles/pages/contact.scss"
 
 const Contact = () => (
   <>
     <SEO title="Contact & CV" />
-    <div id="pt-main" class="pt-perspective">
-      <div class="pt-page pt-page-1">
-        <h1>
-          <span>A collection of</span>
-          <strong>Page 1</strong> Transitions
-        </h1>
-      </div>
-      <div class="pt-page pt-page-2">
-        <h1>
-          <span>A collection of</span>
-          <strong>Page 2</strong> Transitions
-        </h1>
-      </div>
-      <div class="pt-page pt-page-3">
-        <h1>
-          <span>A collection of</span>
-          <strong>Page 3</strong> Transitions
-        </h1>
-      </div>
-    </div>
-    <Menu active="contact"></Menu>
+    <section className="contact">
+      <Menu active="contact"></Menu>
+      <ContactDesc></ContactDesc>
+      <ContactMedia></ContactMedia>
+    </section>
   </>
 )
 
