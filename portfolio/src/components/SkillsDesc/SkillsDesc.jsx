c8ab50548a92637a94940bdabf78357fb7efabc9
Kuba <ja.tanski@gmail.com>
2019-07-14 19:05:01 +0200

    <fetat> skills structure is ready

---
+++ portfolio/src/components/SkillsDesc/SkillsDesc.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import "./skillsDesc.scss"
+
+const SkillsDesc = () => (
+  <div className="desc desc-projects">
+    <h1 className="desc__title">My skills</h1>
+    <div className="desc__desc">
+      <p className="desc__desc__el">Below you can watch my technology stack.</p>
+      <p className="desc__desc__el">
+        I trained all of then in practical project, <br /> which you can watch
+        in section projects.
+      </p>
+      <p className="desc__desc__el">All time I learn and try technology.</p>
+    </div>
+  </div>
+)
+
+export default SkillsDesc
--- portfolio/src/pages/skills.jsx
+++ portfolio/src/pages/skills.jsx
@@ -1,17 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import SEO from "../components/seo"
 import Menu from "../components/Menu/Menu"
+import SkillsDesc from "../components/SkillsDesc/SkillsDesc"
+import {
+  GatsbySVG,
+  GraphQLSVG,
+  MySQLSVG,
+  NextJsSVG,
+  ReduxSVG,
+} from "../svg/icons"
+
+import "../styles/pages/skills.scss"
 
 const Skills = () => (
   <>
     <SEO title="Skills" />
-    <h1>Hi people</h1>
-    <p>On this site you may know to how technology I know.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
-    <Menu active="skills"></Menu>
+    <section className="skills">
+      <div className="skills__info">
+        <Menu active="skills"></Menu>
+        <SkillsDesc></SkillsDesc>
+      </div>
+      <div className="skills__techs">
+        <div className="skills__techs__learned">
+          <h3 className="skills__techs__learned__title">Learned</h3>
+          <div className="skills__learnedIcons">
+            <i className="fab fa-html5 skills-ico"></i>
+            <i className="fab fa-css3-alt skills-ico"></i>
+            <i className="devicon-sass-original skills-ico"></i>
+            <i className="fab fa-js skills-ico"></i>
+            <i className="devicon-react-original-wordmark skills-ico"></i>
+            <GatsbySVG></GatsbySVG>
+            <ReduxSVG></ReduxSVG>
+            <i className="devicon-nodejs-plain-wordmark skills-ico"></i>
+            <i className="devicon-express-original skills-ico"></i>
+            <i className="devicon-mongodb-plain-wordmark skills-ico"></i>
+            <i className="devicon-git-plain skills-ico"></i>
+          </div>
+        </div>
+        <div className="skills__techs__inProgress">
+          <h2 className="skills__techs__inProgress__title">In progress</h2>
+          <div className="skills__inProgressIcons">
+            <i className="devicon-typescript-plain skills-ico"></i>
+            <GraphQLSVG></GraphQLSVG>
+            <NextJsSVG></NextJsSVG>
+            <MySQLSVG></MySQLSVG>
+          </div>
+        </div>
+      </div>
+    </section>
   </>
 )
 
