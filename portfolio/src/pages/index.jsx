d047d53aa51b52b0628e829a9eecf9847076c719
Kuba <ja.tanski@gmail.com>
2019-07-18 00:45:34 +0200

    <fix> little change in style

--- portfolio/src/pages/index.jsx
+++ portfolio/src/pages/index.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
+import { navigate } from "gatsby"
 import { MDBBtn } from "mdbreact"
 
 import SEO from "../components/seo"
@@ -11,23 +11,38 @@ import {
 import "../styles/global.scss"
 import "../styles/pages/index.scss"
 
-const IndexPage = () => {
-  return (
-    <>
-      <SEO title="Welcome" />
-      <section className="loader">
-        <DesktopLoader></DesktopLoader>
-        <MobileLoader></MobileLoader>
-        <div className="loader__startBtn">
-          <Link to="/home">
-            <MDBBtn size="lg" outline color="elegant">
-              Let's start!
-            </MDBBtn>
-          </Link>
+class IndexPage extends Component {
+  state = {
+    className: "loader",
+    bgClassName: "sectionBg",
+  }
+
+  goToHome = () => {
+    const changePage = () => {
+      navigate("/home")
+    }
+    this.setState({ className: "loader goOut", bgClassName: "sectionBg bgOut" })
+    setTimeout(changePage, 1200)
+  }
+
+  render() {
+    return (
+      <>
+        <SEO title="Welcome" />
+        <div className={this.state.bgClassName}>
+          <section className={this.state.className}>
+            <DesktopLoader></DesktopLoader>
+            <MobileLoader></MobileLoader>
+            <div className="loader__startBtn">
+              <MDBBtn onClick={this.goToHome} size="lg" outline color="elegant">
+                Let's start!
+              </MDBBtn>
+            </div>
+          </section>
         </div>
-      </section>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default IndexPage
